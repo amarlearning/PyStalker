cc6538de28bf51610f3e9b826874f4c95bfb6f08
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2016-04-01 18:43:24 +0530

    adding more function :tiger:

--- stalker.py
+++ stalker.py
@@ -2,15 +2,15 @@ import urllib
 import re
 from bs4 import BeautifulSoup
 
-def print_in_style(catch) :
-    print "--------------------------"
-    print "| Last visit : " +catch[10:]+" |"
-    print "--------------------------"
+# def print_in_style(catch) :
+#     print "--------------------------"
+#     print "| Last visit : " +catch[10:]+" |"
+#     print "--------------------------"
 
-def generate_url() :
-    username = raw_input("Stalker! You know the username of that guy, enter it : ")
-    url = "http://codeforces.com/profile/" + username
-    return url
+# def generate_url() :
+#     username = raw_input("Stalker! You know the username of that guy, enter it : ")
+#     url = "http://codeforces.com/profile/" + username
+#     return url
 
 def space() :
     print ""
@@ -22,36 +22,44 @@ def menu() :
     print ""
     index = raw_input("Enter the index : ")
     return index
+
+def call_defined_function(value) :
+    if value is 1 :
+        # call_check_username_function()
+    if value is 2 :
+        # call_all_saved_name()
+    else
+        print "You entered wrong index, Try again!"
     
 
-def find_what_you_are_looking_for(scrapper) :
-    matches = scrapper.find('div',{'id':'pageContent'})
-    match = matches.find('div',{'class':'roundbox'})
-    match = match.find('ul')
-    for li in match.find_all('li') :
-        catch = li.get_text()
-        if "Last visit" in catch :
-            catch = catch.strip()
-            catch = re.sub(r"\s", "", catch)
-            print_in_style(catch)
+# def find_what_you_are_looking_for(scrapper) :
+#     matches = scrapper.find('div',{'id':'pageContent'})
+#     match = matches.find('div',{'class':'roundbox'})
+#     match = match.find('ul')
+#     for li in match.find_all('li') :
+#         catch = li.get_text()
+#         if "Last visit" in catch :
+#             catch = catch.strip()
+#             catch = re.sub(r"\s", "", catch)
+#             print_in_style(catch)
 
-def get_webpage_data(webpage) :
-    scrapper = BeautifulSoup(webpage , 'html.parser')
-    matches = scrapper.prettify()
-    find_what_you_are_looking_for(scrapper)
+# def get_webpage_data(webpage) :
+#     scrapper = BeautifulSoup(webpage , 'html.parser')
+#     matches = scrapper.prettify()
+#     find_what_you_are_looking_for(scrapper)
 
-def check_connection() :
-    response = urllib.urlopen(generate_url())
-    if response == None :
-        print "Check your Internet conneciton! Not working."
-    else :
-        get_webpage_data(response)
+# def check_connection() :
+#     response = urllib.urlopen(generate_url())
+#     if response == None :
+#         print "Check your Internet conneciton! Not working."
+#     else :
+#         get_webpage_data(response)
 
 def main() :
     space()
     print "\t\t \tPYSTALKER\t\t"
     space()
-    value = menu()
+    call_defined_function(menu())
     
 if __name__ == "__main__" :
     main()
