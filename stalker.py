3908fd41c27b5f28a553ddcc5415deafe60b59e9
Amar Pandey <amar.om1994@gmail.com>
2016-04-04 22:36:39 +0530

    stage one done :octocat:

--- stalker.py
+++ stalker.py
@@ -7,10 +7,10 @@ from bs4 import BeautifulSoup
 #     print "| Last visit : " +catch[10:]+" |"
 #     print "--------------------------"
 
-# def generate_url() :
-#     username = raw_input("Stalker! You know the username of that guy, enter it : ")
-#     url = "http://codeforces.com/profile/" + username
-#     return url
+def generate_url() :
+    username = raw_input("Stalker! You know the username of that guy, enter it : ")
+    url = "http://codeforces.com/profile/" + username
+    return url
 
 def space() :
     print ""
@@ -26,34 +26,37 @@ def menu() :
 def call_defined_function(value) :
     if value is 1 :
         # call_check_username_function()
+        print ""
+        check_connection()
     if value is 2 :
         # call_all_saved_name()
-    else
+        
+    else :
         print "You entered wrong index, Try again!"
     
 
-# def find_what_you_are_looking_for(scrapper) :
-#     matches = scrapper.find('div',{'id':'pageContent'})
-#     match = matches.find('div',{'class':'roundbox'})
-#     match = match.find('ul')
-#     for li in match.find_all('li') :
-#         catch = li.get_text()
-#         if "Last visit" in catch :
-#             catch = catch.strip()
-#             catch = re.sub(r"\s", "", catch)
-#             print_in_style(catch)
+def find_what_you_are_looking_for(scrapper) :
+    matches = scrapper.find('div',{'id':'pageContent'})
+    match = matches.find('div',{'class':'roundbox'})
+    match = match.find('ul')
+    for li in match.find_all('li') :
+        catch = li.get_text()
+        if "Last visit" in catch :
+            catch = catch.strip()
+            catch = re.sub(r"\s", "", catch)
+            print_in_style(catch)
 
-# def get_webpage_data(webpage) :
-#     scrapper = BeautifulSoup(webpage , 'html.parser')
-#     matches = scrapper.prettify()
-#     find_what_you_are_looking_for(scrapper)
+def get_webpage_data(webpage) :
+    scrapper = BeautifulSoup(webpage , 'html.parser')
+    matches = scrapper.prettify()
+    find_what_you_are_looking_for(scrapper)
 
-# def check_connection() :
-#     response = urllib.urlopen(generate_url())
-#     if response == None :
-#         print "Check your Internet conneciton! Not working."
-#     else :
-#         get_webpage_data(response)
+def check_connection() :
+    response = urllib.urlopen(generate_url())
+    if response == None :
+        print "Check your Internet conneciton! Not working."
+    else :
+        get_webpage_data(response)
 
 def main() :
     space()
