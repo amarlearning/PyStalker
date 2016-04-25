81e7540d1f308990a1152ef14d500fd665251973
Amar Prakash Pandey <amarlearning@users.noreply.github.com>
2016-04-26 02:11:01 +0530

    typo mistake

--- stalker.py
+++ stalker.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Author: Amar Prakash Pandey
-# @Date:   2016-03-20
+# @Date:   2016-03-24
 # @Email:  amar.om1994@gmail.com  
 # @Github username: @amarlearning
 # @Last Modified by: Amar Prakash Pandey  
-# @Last Modified date: 2016-04-17
+# @Last Modified date: 2016-04-26
 # MIT License. You can find a copy of the License
 # @http://amarlearning.mit-license.org
 
