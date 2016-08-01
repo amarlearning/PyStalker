8f632078b911850302329faed2a5a964eb54e1d4
Amar Prakash Pandey <amar.om1994@gmail.com>
2016-08-01 11:35:48 +0530

    fixing linux issue

--- stalker.py
+++ stalker.py
@@ -4,7 +4,7 @@
 # @Email:  amar.om1994@gmail.com  
 # @Github username: @amarlearning
 # @Last Modified by: Amar Prakash Pandey  
-# @Last Modified date: 2016-04-26
+# @Last Modified date: 2016-08-01
 # MIT License. You can find a copy of the License
 # @http://amarlearning.mit-license.org
 
